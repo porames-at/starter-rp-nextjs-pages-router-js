d67f89e2445446c44f492507fde0a90618dbf087
porames-at <porames.at@logicspark.com>
2025-12-19 16:29:43 +0700

    Update package.json to downgrade React and ReactDOM to version 18.2.0 and upgrade Next.js to version 15.5.9; remove commented HTML structure from RootLayout component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,10 +3,6 @@ import { LazyAppProviders } from "./LazyAppProviders";
 export default function RootLayout({ children }) {
   return (
     <>
-      {/* <html lang="en">
-        <body className={"antialiased"}>
-        </body>
-      </html> */}
       <LazyAppProviders licenseKey="your-license-key">
         <main>{children}</main>
       </LazyAppProviders>
