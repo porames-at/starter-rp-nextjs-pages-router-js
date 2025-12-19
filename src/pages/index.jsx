f804b6c21a14236c6409395f8551f489055f2327
porames-at <porames.at@logicspark.com>
2025-12-19 14:02:54 +0700

    Add loading state to Home component for LazyAppPdfViewer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,23 @@
+"use client";
 import { LazyAppPdfViewer } from "@/components/LazyAppPdfViewer";
 
 export default function Home() {
+  const [ready, setReady] = useState(false);
+
+useEffect(() => {
+  const id = setTimeout(() => setReady(true), 2000);
+  return () => clearTimeout(id);
+}, []);
   return (
     <div className="w-[1028px] h-[700px] mx-auto">
       <h1>RP Starter Toolkit: Nextjs + Pages Router + Javascript</h1>
       <br />
       <h2>Default Toolbar</h2>
-      <LazyAppPdfViewer />
+      {ready && <LazyAppPdfViewer />}
       <h2>Without Toolbar</h2>
-      <LazyAppPdfViewer showToolbar={false} />
+      {ready && <LazyAppPdfViewer showToolbar={false} />}
       <h2>Mobile</h2>
-      <LazyAppPdfViewer defaultLayoutProps={{ style: { width: "500px" } }} />
+      {ready && <LazyAppPdfViewer defaultLayoutProps={{ style: { width: "500px" } }} />}
     </div>
   )
 }
