1ac1e47c9d0285f67bdbcead68af301979158c81
porames-at <porames.at@logicspark.com>
2025-12-19 16:13:00 +0700

    Reorganize RootLayout component structure and comment out HTML tags

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,13 +3,13 @@ import { LazyAppProviders } from "./LazyAppProviders";
 export default function RootLayout({ children }) {
   return (
     <>
-      <html lang="en">
+      {/* <html lang="en">
         <body className={"antialiased"}>
-          <LazyAppProviders licenseKey="your-license-key">
-            <main>{children}</main>
-          </LazyAppProviders>
         </body>
-      </html>
+      </html> */}
+      <LazyAppProviders licenseKey="your-license-key">
+        <main>{children}</main>
+      </LazyAppProviders>
     </>
   );
 }
